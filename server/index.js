4d73aa06c8e7c5bc61dae3f8e8b298d803599fdb
dubKelly <js.neeb1780@gmail.com>
2018-10-19 23:36:17 -0400

    ...

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.use('/api/employees', employees);
 
 // Serve static in production
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('../build'));
+	app.use(express.static(path.join(__dirname, '../build')));
 
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
