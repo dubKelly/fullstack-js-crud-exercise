81539c0615e96be8173fabf8710fee9f68a1ea81
dubKelly <js.neeb1780@gmail.com>
2018-10-20 10:28:59 -0400

    cors

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 // };
 
 app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 	res.setHeader('Access-Control-Allow-Credentials', 'true');
 	res.setHeader(
 		'Access-Control-Allow-Methods',
