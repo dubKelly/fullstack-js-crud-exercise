189045b67f6057672beaa16135e7ebf12c758aa0
dubKelly <js.neeb1780@gmail.com>
2018-10-22 01:20:14 -0400

    build

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use('/api/employees', cors(corsOptions), employees);
 
 // Serve static in production
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('/build'));
+	app.use(express.static('build'));
 
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
